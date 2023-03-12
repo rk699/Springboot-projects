9cfc4cd94d206bba8bcc2b0a4e4003441c1cf329
Purusottam Gupta <55584809+rk699@users.noreply.github.com>
2023-03-12 12:45:08 +0530

    Add files via upload
    file-upload

---
+++ spring-boot-fileupload/src/main/java/com/purusottam/springbootfileupload/Controller/FileUploadController.java
@@ -0,0 +1,36 @@
+package com.purusottam.springbootfileupload.Controller;
+
+import java.io.IOException;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.purusottam.springbootfileupload.fileupload.FileuploadResponse;
+import com.purusottam.springbootfileupload.fileuploadUtil.FileUploadUtil;
+ 
+@RestController
+public class FileUploadController {
+     
+    @PostMapping("/uploadFile")
+    public ResponseEntity<FileuploadResponse> uploadFile(
+            @RequestParam("file") MultipartFile multipartFile)
+                    throws IOException {
+         
+        String fileName = StringUtils.cleanPath(multipartFile.getOriginalFilename());
+        long size = multipartFile.getSize();
+         
+        String filecode = FileUploadUtil.saveFile(fileName, multipartFile);
+         
+        FileuploadResponse response = new FileuploadResponse();
+        response.setFileName(fileName);
+        response.setSize(size);
+        response.setDownloadUri("/downloadFile/" + filecode);
+         
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+}
---
+++ spring-boot-fileupload/src/main/java/com/purusottam/springbootfileupload/fileupload/FileuploadResponse.java
@@ -0,0 +1,28 @@
+package com.purusottam.springbootfileupload.fileupload;
+
+public class FileuploadResponse {
+
+	 private String fileName;
+	    private String downloadUri;
+	    private long size;
+	    
+	    
+		public String getFileName() {
+			return fileName;
+		}
+		public void setFileName(String fileName) {
+			this.fileName = fileName;
+		}
+		public String getDownloadUri() {
+			return downloadUri;
+		}
+		public void setDownloadUri(String downloadUri) {
+			this.downloadUri = downloadUri;
+		}
+		public long getSize() {
+			return size;
+		}
+		public void setSize(long size) {
+			this.size = size;
+		}
+}
